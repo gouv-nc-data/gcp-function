d489f820974d85b07bdcdb2afdab53643650d52f
abugeia <antoine.bugeia@gouv.nc>
2024-01-30 14:10:09 +1100

    feat : ajout du vpc et ip en output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "run_url" {
 output "function_sa_email" {
   value = google_service_account.service_account.email
 }
+
+output "vpc_network_name" {
+  value = google_compute_network.vpc_network.name
+}
+
+output "ip" {
+  value = google_compute_address.default[0].self_link
+}

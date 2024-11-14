3f70e818ce6c1418a182209f7a3be0ec936b78d8
Thomas Guyader <thomas.guyader@gouv.nc>
2024-11-14 15:10:51 +1100

    chore(vcp connector): correction référence réseau

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ locals {
     } : var.enable_vpn ? {
     ip_cidr_range = "10.10.10.0/28"
     name    = "vpc-connector"
-    network = "subnet-for-vpn"
+    network = "vpc-${var.project_name}"
   } : null
 
   revision_annotations = var.ip_fixe ? {

1bd31eb9a363ed593a226cb97582a7961d19ecf4
Thomas Guyader <thomas.guyader@gouv.nc>
2024-11-14 11:22:01 +1100

    chore(env): ajout tomap

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ module "google_cloud_run" {
           cpu    = var.cpu_limits
         }
       }
-      env = var.env
+      env = tomap(var.env)
     }
   }
   vpc_connector_create = local.local_vpc_connector
